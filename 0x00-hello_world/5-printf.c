72d891e970a92d81455c3e75d72d39e9f0303bf9
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-02 10:20:40 +0100

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints a sample string
+ * Return : if the program execute properly, else a non-xero number
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a poece of art, \n");
+	return (0);
+}
