8aeb412adb16fda7ff3b02743bf278c1d7dfbf24
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-01-17 16:25:28 +0000

    recurse it

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and
+ returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    response = requests.get(url, headers=headers)
+
+    """Check if request is successful"""
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print(data)
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
+        except KeyError:
+            return 0
+    else:
+        return 0
